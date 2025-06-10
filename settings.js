857c3273bff6e8458685bcaa854ef8da1d0b2c1e
Madumaxa <danumax193@gmail.com>
2025-06-10 19:09:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=J94jzRJJ#4dKsmsFs_V2uC67J41IUonKImoOzz9qbWpoSoJyT6nk",
+SESSION_ID: process.env.SESSION_ID || "Lakiya-Md=eAEAQKKb#7JHDKJh9UYOPZJYVQFGGx1z4eJ5qbaE8klH0k7xG7fk",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
